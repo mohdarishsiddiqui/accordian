049d3983a2dba430f89b3cdfc6638fab90a929f6
mohdarishsiddiqui <arishsiddiqui5@gmail.com>
2024-02-16 18:45:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Accordian from '.'
+
+function App() {
+
+
+  return (
+    <Accordian></Accordian>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,25 @@
+const data = [
+  {
+    id:"1",
+    question:"what is accordian component ?",
+    answer:"The accordion component is used to generate vertically stacked content in a page. It is intended to save vertical space by hiding content, reducing scrolling. Accordions contains labels with the relevant information. "
+  },
+  {
+    id:"2",
+    question:"what are they used for ?",
+    answer:"The accordion component is used to generate vertically stacked content in a page."
+  },
+  {
+    id:"3",
+    question:"Accodion as music instrument",
+    answer:"Accordions are a family of box-shaped musical instruments of the bellows-driven free reed aerophone type "
+  },
+  {
+    id:"4",
+    question:"Can i create accordion component in framework?",
+    answer:"'Yes of course, it is very possible to create an accordion component with another framework"
+  },
+];
+
+export default data;
+
